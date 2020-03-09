c69296ab46d11f4012d8532de71bcdad2d4a1abd
Modervital <52286231+Modervital@users.noreply.github.com>
2020-03-09 13:54:34 +0200

    Add files via upload
    test

---
+++ games/Ukraine/units/commsupport1.lua
@@ -0,0 +1,209 @@
+return { commsupport1 = {
+  unitname            = [[commsupport1]],
+  name                = [[Support Commander]],
+  description         = [[Econ/Support Commander, Builds at 12 m/s]],
+  acceleration        = 0.75,
+  activateWhenBuilt   = true,
+  autoHeal            = 5,
+  brakeRate           = 2.7,
+  buildCostMetal      = 13200,
+  buildDistance       = 2530,
+  builder             = true,
+
+  buildoptions        = {
+  },
+
+  buildPic            = [[commsupport.png]],
+  canGuard            = true,
+  canMove             = true,
+  canPatrol           = true,
+  category            = [[LAND]],
+  collisionVolumeOffsets = [[0 0 0]],
+  collisionVolumeScales  = [[45 50 45]],
+  collisionVolumeType    = [[CylY]],
+  corpse              = [[DEAD]],
+
+  customParams        = {
+    level = [[1]],
+    statsname = [[dynsupport1]],
+    soundok = [[heavy_bot_move]],
+    soundselect = [[bot_select]],
+    soundbuild = [[builder_start]],
+    commtype = [[4]],
+    aimposoffset   = [[0 15 0]],
+  },
+
+  energyMake          = 63,
+  energyStorage       = 5030,
+  energyUse           = 0,
+  explodeAs           = [[ESTOR_BUILDINGEX]],
+  footprintX          = 2,
+  footprintZ          = 2,
+  iconType            = [[commander1]],
+  idleAutoHeal        = 5,
+  idleTime            = 1800,
+  leaveTracks         = true,
+  losEmitHeight       = 40,
+  maxDamage           = 2000,
+  maxSlope            = 36,
+  maxVelocity         = 1.2,
+  maxWaterDepth       = 5000,
+  metalMake           = 4,
+  metalStorage        = 500,
+  minCloakDistance    = 75,
+  movementClass       = [[AKBOT2]],
+  noChaseCategory     = [[TERRAFORM FIXEDWING GUNSHIP HOVER SHIP SWIM SUB LAND FLOAT SINK TURRET]],
+  objectName          = [[commsupport.s3o]],
+  script              = [[commsupport.lua]],
+  selfDestructAs      = [[ESTOR_BUILDINGEX]],
+
+  sfxtypes            = {
+
+    explosiongenerators = {
+      [[custom:flashmuzzle1]],
+      [[custom:NONE]],
+    },
+
+  },
+
+  showNanoSpray       = false,
+  showPlayerName      = true,
+  sightDistance       = 500,
+  sonarDistance       = 300,
+  trackOffset         = 0,
+  trackStrength       = 8,
+  trackStretch        = 1,
+  trackType           = [[ComTrack]],
+  trackWidth          = 22,
+  turnRate            = 1350,
+  upright             = true,
+  workerTime          = 12,
+
+  weapons             = {
+
+    [1] = {
+      def                = [[FAKELASER]],
+      badTargetCategory  = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+  
+    [5] = {
+      def                = [[GAUSS]],
+      badTargetCategory  = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+
+  },
+
+
+  weaponDefs          = {
+
+    FAKELASER     = {
+      name                    = [[Fake Laser]],
+      areaOfEffect            = 12,
+      beamTime                = 0.1,
+      coreThickness           = 0.5,
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      customParams            = {
+        bogus = 1,
+      },
+
+      damage                  = {
+        default = 0,
+      },
+
+      duration                = 0.1,
+      explosionGenerator      = [[custom:flash1green]],
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      laserFlareSize          = 5.53,
+      minIntensity            = 1,
+      range                   = 450,
+      reloadtime              = 0.1,
+      rgbColor                = [[0 1 0]],
+      sweepfire               = false,
+      texture1                = [[largelaser]],
+      texture2                = [[flare]],
+      texture3                = [[flare]],
+      texture4                = [[smallflare]],
+      thickness               = 5.53,
+      tolerance               = 10000,
+      turret                  = true,
+      weaponType              = [[BeamLaser]],
+      weaponVelocity          = 900,
+    },
+    
+    GAUSS = {
+      name                    = [[Gauss Rifle]],
+      alphaDecay              = 0.12,
+      areaOfEffect            = 16,
+      avoidfeature            = false,
+      bouncerebound           = 0.15,
+      bounceslip              = 1,
+      cegTag                  = [[gauss_tag_l]],
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      damage                  = {
+        default = 140,
+        planes  = 140,
+        subs    = 8,
+      },
+      
+      customParams = {
+        single_hit = true,
+      },
+
+      explosionGenerator      = [[custom:gauss_hit_l]],
+      groundbounce            = 1,
+      impactOnly              = true,
+      impulseBoost            = 0,
+      impulseFactor           = 0,
+      interceptedByShieldType = 0,
+      noExplode               = true,
+      noSelfDamage            = true,
+      numbounce               = 40,
+      range                   = 420,
+      reloadtime              = 2.5,
+      rgbColor                = [[0.5 1 1]],
+      separation              = 0.5,
+      size                    = 0.8,
+      sizeDecay               = -0.1,
+      soundHit                = [[weapon/gauss_hit]],
+      soundHitVolume          = 3,
+      soundStart              = [[weapon/gauss_fire]],
+      soundStartVolume        = 2.5,
+      stages                  = 32,
+      turret                  = true,
+      waterbounce             = 1,
+      weaponType              = [[Cannon]],
+      weaponVelocity          = 2200,
+    },
+
+  },
+
+
+  featureDefs         = {
+
+    DEAD      = {
+      blocking         = true,
+      featureDead      = [[HEAP]],
+      footprintX       = 2,
+      footprintZ       = 2,
+      object           = [[commsupport_dead.s3o]],
+    },
+
+    HEAP      = {
+      blocking         = false,
+      footprintX       = 2,
+      footprintZ       = 2,
+      object           = [[debris2x2c.s3o]],
+    },
+
+  },
+
+} }
